33addc74a5cb80e1941baa235d98e164c6288773
onallera86 <onallera86@gmail.com>
2026-09-06 21:48:54 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Juegos El Jarocho
+
+Sube estos archivos a GitHub y despliega en Render como Web Service.
+Build: npm install
+Start: npm start
---
+++ server.js
@@ -0,0 +1,1 @@
+const express=require('express');const app=express();app.use(express.static('public'));const port=process.env.PORT||3000;app.listen(port,()=>console.log('Juegos El Jarocho en '+port));
